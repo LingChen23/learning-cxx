1cad50766ec75492059fbc5e92911392cc12626b
yy <1667253917@qq.com>
2024-08-07 01:03:44 +0800

    修改:21

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -18,6 +18,7 @@ struct Tensor {
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
+
     ~Tensor() {
         delete[] data;
     }
@@ -29,6 +30,7 @@ struct Tensor {
     T &operator[](unsigned int const indices[N]) {
         return data[data_index(indices)];
     }
+
     T const &operator[](unsigned int const indices[N]) const {
         return data[data_index(indices)];
     }
@@ -36,17 +38,17 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
-        unsigned  int stride = 1;
+        unsigned int stride = 1;
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
             stride = 1;
-            for(unsigned int j = i + 1; j <N; ++j){
+            for (unsigned int j = i + 1; j < N; ++j) {
                 stride *= shape[j];
             }
-
             index += stride * indices[i];
         }
+        return index;
     }
 };
 
@@ -59,10 +61,12 @@ int main(int argc, char **argv) {
         unsigned int i0[]{0, 0, 0, 0};
         tensor[i0] = 1;
         ASSERT(tensor[i0] == 1, "tensor[i0] should be 1");
+        ASSERT(tensor.data[0] == 1, "tensor[i0] should be 1");
 
         unsigned int i1[]{1, 2, 3, 4};
-        tensor[i0] = 2;
-        ASSERT(tensor[i0] == 2, "tensor[i1] should be 2");
+        tensor[i1] = 2;
+        ASSERT(tensor[i1] == 2, "tensor[i1] should be 2");
+        ASSERT(tensor.data[119] == 2, "tensor[i1] should be 2");
     }
     {
         unsigned int shape[]{7, 8, 128};
@@ -71,10 +75,12 @@ int main(int argc, char **argv) {
         unsigned int i0[]{0, 0, 0};
         tensor[i0] = 1.f;
         ASSERT(tensor[i0] == 1.f, "tensor[i0] should be 1");
+        ASSERT(tensor.data[0] == 1.f, "tensor[i0] should be 1");
 
         unsigned int i1[]{3, 4, 99};
-        tensor[i0] = 2.f;
-        ASSERT(tensor[i0] == 2.f, "tensor[i1] should be 2");
+        tensor[i1] = 2.f;
+        ASSERT(tensor[i1] == 2.f, "tensor[i1] should be 2");
+        ASSERT(tensor.data[3683] == 2.f, "tensor[i1] should be 2");
     }
     return 0;
 }
